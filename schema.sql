3aeaf8bedc83b994f1a287ff990febe8b6e81af4
Sandro Donizeti Gomes <129443003+web-sandro@users.noreply.github.com>
2025-09-27 01:35:06 -0300

    mysql

--- schema.sql
+++ schema.sql
@@ -11,11 +11,18 @@ CREATE TABLE IF NOT EXISTS selection_items (
     id INT AUTO_INCREMENT PRIMARY KEY,
     selection_id INT NOT NULL,
     num INT NOT NULL,
-    event ENUM('right','left','up','down','back') NOT NULL,
+    event ENUM(
+        'right',
+        'left',
+        'up',
+        'down',
+        'back',
+        'upRight',
+        'upLeft',
+        'downRight',
+        'downLeft'
+    ) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (selection_id) REFERENCES selections(id) ON DELETE CASCADE,
     UNIQUE KEY uniq_selection_num (selection_id, num) -- evita duplicar número na mesma seleção
 );
-
-SELECT * FROM grid_app.selections;
-SELECT * FROM grid_app.selection_items;
